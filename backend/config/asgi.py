a32213e6cb061b1a83f60904414d85fe13d7627c
Allan Monteiro <engflorestal.allan@gmail.com>
2026-03-29 12:28:38 -0300

    Fix ASGI startup import order

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -2,11 +2,12 @@ import os
 import django
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from config.routing import websocket_urlpatterns
-from config.ws_auth import JwtQueryAuthMiddlewareStack
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 django.setup()
+
+from config.routing import websocket_urlpatterns
+from config.ws_auth import JwtQueryAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
